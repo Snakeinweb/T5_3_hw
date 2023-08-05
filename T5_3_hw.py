8aa43078bf3699e37fe8ad949bee1e7ccd744c81
Kirill <100080763+Snakeinweb@users.noreply.github.com>
2023-08-05 18:16:29 +0700

    Добавьте файлы проекта.

---
+++ T5_3_hw.py
@@ -0,0 +1,15 @@
+﻿
+invest = float(input('Сумма инвестиций: ')) 
+mike = float(input('У Майка: '))
+ivan = float(input('У Ивана: '))
+
+if invest <= mike + ivan and invest <= mike and invest <= ivan: 
+    print ('2')
+elif invest <= mike + ivan and invest > mike and invest > ivan:
+    print ('1')
+elif invest <= mike:
+    print ('Mike')
+elif invest <= ivan:
+    print ('Ivan')
+else:
+    print ('0')
